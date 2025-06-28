61b901961065672621a207334a21bdb18f37ef9b
xie7654 <765462425@qq.com>
2025-06-28 11:29:05 +0800

    0.0.5 修改access_token 为app_secret,更新README.md

--- goofish_api/__init__.py
+++ goofish_api/__init__.py
@@ -4,8 +4,8 @@ from goofish_api.utils.base_client import BaseClient
 
 class GoofishClient(BaseClient):
 
-    def __init__(self,  app_key, access_token, debug=False):
-        super().__init__(app_key, access_token, debug)
+    def __init__(self,  app_key, app_secret, debug=False):
+        super().__init__(app_key, app_secret, debug)
         self.user = api.User(self)
         self.good = api.Good(self)
         self.order = api.Order(self)
